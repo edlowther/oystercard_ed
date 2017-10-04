79c043390957a20611f3bcaf89f9df29a52e29a6
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-04 14:35:18 +0100

    Tweaked feature test to check for incomplete journey

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,11 +3,16 @@ require 'oystercard'
 feature 'Getting started' do
   let(:brixton) { double(:station, :name => "Brixton") }
   let(:pimlico) { double(:station, :name => "Pimlico") }
+  let(:card) { Oystercard.new }
+  before(:each) { card.top_up 10 }
   it 'shows journey history' do
-    card = Oystercard.new
-    card.top_up 20
     card.touch_in brixton
     card.touch_out pimlico
+    p card.history
+  end
+  it 'registers an incomplete journey if customer fails to touch out' do
+    card.touch_in brixton
+    card.touch_in pimlico
     p card.history
   end
 end
