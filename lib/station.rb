5e977c8e277b43c7f58190fd87a72d92b8c33f0d
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-04 14:26:43 +0100

    Added zones to station class

--- lib/station.rb
+++ lib/station.rb
@@ -1,6 +1,7 @@
 class Station
-  attr_reader :name
-  def initialize name
+  attr_reader :name, :zone
+  def initialize name, zone
     @name = name
+    @zone = zone
   end
 end
--- spec/units/station_spec.rb
+++ spec/units/station_spec.rb
@@ -2,7 +2,11 @@ require 'station'
 
 describe Station do
   it "can take a name" do
-    seven_sisters = Station.new "Seven Sisters"
+    seven_sisters = Station.new "Seven Sisters", 3
     expect(seven_sisters.name).to eq "Seven Sisters"
+  end
+  it "can be assigned to a zone" do
+    seven_sisters = Station.new "Pimlico", 1
+    expect(seven_sisters.zone).to eq 1
   end
 end
