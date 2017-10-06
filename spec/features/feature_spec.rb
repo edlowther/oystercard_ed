bc0acfcdf5b277906aad6f5decf73d5f507da4a9
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-06 16:26:14 +0100

    Improved feature file

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,18 +1,26 @@
 require 'oystercard'
+require 'station'
 
-feature 'Getting started' do
-  let(:brixton) { double(:station, :name => "Brixton") }
-  let(:pimlico) { double(:station, :name => "Pimlico") }
+feature 'Card is usable' do
+  let(:brixton) { Station.new "Brixton", 2 }
+  let(:pimlico) { Station.new "Pimlico", 1 }
   let(:card) { Oystercard.new }
   before(:each) { card.top_up 10 }
-  # it 'shows journey history' do
-  #   card.touch_in brixton
-  #   card.touch_out pimlico
-  #   p card.history
-  # end
-  # it 'registers an incomplete journey if customer fails to touch out' do
-  #   card.touch_in brixton
-  #   card.touch_in pimlico
-  #   p card.history
-  # end
+  it 'shows journey history' do
+    card.touch_in brixton
+    card.touch_out pimlico
+    card.touch_in pimlico
+    card.touch_out brixton
+    p card.journeylog.journeys
+  end
+  it 'deducts correct fare if touched in and out' do
+    card.touch_in brixton
+    card.touch_out pimlico
+    p card.balance
+  end
+  it 'deducts a penalty fare if customer fails to touch out' do
+    card.touch_in brixton
+    card.touch_in pimlico
+    p card.balance
+  end
 end
